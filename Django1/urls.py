415be09942693f0c4cd2cb34b8911c9bf7d4ae6d
kero33 <kerokop87@gmail.com>
2022-04-04 16:23:15 +0200

    project1

--- Django1/urls.py
+++ Django1/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from app1.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home,name='home'),
+    path('addQuestion/', addQuestion,name='addQuestion'),
+    path('login/', loginPage,name='login'),
+    path('logout/', logoutPage,name='logout'),
+    path('register/', registerPage,name='register'),
+    
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
